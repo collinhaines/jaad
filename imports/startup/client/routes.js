1be5439e337a65ff5619d6d6beb626c9bdadd060
Collin Haines <collinhaines@me.com>
2016-09-01 09:38:29 -0400

    Dynamic Main Content

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -6,12 +6,15 @@ import '/imports/ui/components/navigation.js';
 import '/imports/ui/components/sidebar.js';
 import '/imports/ui/components/footer.js';
 
-// Load template.
-import '/imports/ui/pages/dashboard.js';
+// Load layout.
+import '/imports/ui/layouts/app-body.js';
+
+// Load pages.
+import '/imports/ui/pages/';
 
 FlowRouter.route('/', {
   name: 'Home',
   action() {
-    BlazeLayout.render('dashboard');
+    BlazeLayout.render('app-body', { main: 'dashboard' });
   }
 });
