be646e1b80fd5e280464731647731b050aa974c8
Collin Haines <collinhaines@me.com>
2016-09-01 11:10:40 -0400

    Added Basic Page Templates

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -10,11 +10,82 @@ import '/imports/ui/components/footer.js';
 import '/imports/ui/layouts/overlord.js';
 
 // Load pages.
-import '/imports/ui/pages/';
+import '/imports/ui/pages';
 
 FlowRouter.route('/', {
-  name: 'Home',
   action() {
     BlazeLayout.render('overlord', { main: 'dashboard' });
+  }
+});
+
+FlowRouter.route('/calendar', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'calendar' });
+  }
+});
+
+FlowRouter.route('/mailbox', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'mailbox' });
+  }
+});
+
+FlowRouter.route('/forms', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'forms' });
+  }
+});
+
+FlowRouter.route('/graphs', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'graphs' });
+  }
+});
+
+FlowRouter.route('/tables', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'tables' });
+  }
+});
+
+FlowRouter.route('/alerts', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'alerts' });
+  }
+});
+
+FlowRouter.route('/grid', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'grid' });
+  }
+});
+
+FlowRouter.route('/icons', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'icons' });
+  }
+});
+
+FlowRouter.route('/typography', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'typography' });
+  }
+});
+
+FlowRouter.route('/404', {
+  action() {
+    BlazeLayout.render('overlord', { main: '404' });
+  }
+});
+
+FlowRouter.route('/500', {
+  action() {
+    BlazeLayout.render('overlord', { main: '500' });
+  }
+});
+
+FlowRouter.route('/login', {
+  action() {
+    BlazeLayout.render('overlord', { main: 'login' });
   }
 });
--- imports/ui/pages/index.js
+++ imports/ui/pages/index.js
@@ -1,1 +1,20 @@
+// Overview
+import './calendar.js';
 import './dashboard.js';
+import './mailbox.js';
+
+// Layouts
+import './forms.js';
+import './graphs.js';
+import './tables.js';
+
+// Elements
+import './alerts.js';
+import './grid.js';
+import './icons.js';
+import './typography.js';
+
+// Pages
+import './404.js';
+import './500.js';
+import './login.js';
