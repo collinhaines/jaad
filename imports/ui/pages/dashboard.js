5a635f370588572ae1cf1bff0511828c5a06112f
Collin Haines <collinhaines@me.com>
2016-09-07 09:31:54 -0400

    Customize FullCalendar

--- imports/ui/pages/dashboard.js
+++ imports/ui/pages/dashboard.js
@@ -29,5 +29,41 @@ Template.dashboard.helpers({
 
   renderTime(timestamp) {
     return new Date(timestamp).toLocaleDateString();
+  },
+
+  calendarOptions() {
+    return {
+      defaultView: 'agendaDay',
+
+      // Capitalize visual text.
+      buttonText: {
+        today:  'Today',
+        week:   'Week',
+        day:    'Day'
+      },
+
+      // Reorganize Header.
+      header: {
+        left:    'agendaDay, agendaWeek',
+        center:  'title',
+        right:   'today, prev, next'
+      }
+    }
   }
 });
+
+Template.dashboard.onRendered(function dashboardRendered() {
+  // Override FullCalendar's default look to match the flat look of the dashboard.
+  $('.fc-toolbar button').each(function () {
+    $(this)
+      .removeClass('fc-state-default fc-button fc-corner-left fc-corner-right')
+      .addClass('btn btn-primary');
+
+    if ($(this).hasClass('fc-prev-button')) {
+      $(this).html('<i class="fa fa-angle-left"></i>');
+    } else if ($(this).hasClass('fc-next-button')) {
+      $(this).html('<i class="fa fa-angle-right"></i>');
+    }
+  });
+});
+
