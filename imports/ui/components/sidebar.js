3cf18a663cc58aeaa77befefba58b20e8818d399
Collin Haines <collinhaines@me.com>
2016-09-08 10:38:12 -0400

    Sidebar: a:hover and .active add/fix

--- imports/ui/components/sidebar.js
+++ imports/ui/components/sidebar.js
@@ -1,1 +1,23 @@
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
 import './sidebar.html';
+
+Template.sidebar.onRendered(function sidebarRendered() {
+  // When rendered add the active class.
+  $('li:not(.nav-category) > a[href="' + FlowRouter.current().route.path + '"]')
+    .parent()
+    .addClass('active');
+});
+
+Template.sidebar.events({
+  'click li:not(.nav-category) > a'(event) {
+    // Remove old active.
+    $('li.active').removeClass('active');
+
+    // Add new active.
+    $(event.target)
+      .parent()
+      .addClass('active');
+  }
+});
