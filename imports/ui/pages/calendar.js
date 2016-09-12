41cbd9cbd36b1e4d60c98289232c687a8b6ad48d
Collin Haines <collinhaines@me.com>
2016-09-12 12:27:02 -0400

    Added Calendar

--- imports/ui/pages/calendar.js
+++ imports/ui/pages/calendar.js
@@ -1,1 +1,43 @@
+//
+// Full Calendar: https://fullcalendar.io/
+//
+
 import './calendar.html';
+
+import { Template } from 'meteor/templating';
+
+Template.calendar.helpers({
+  configuration() {
+    return {
+      // Capitalize visual text.
+      buttonText: {
+        today: 'Today',
+        month: 'Month',
+        week:  'Week',
+        day:   'Day'
+      },
+
+      // Reorganize Header.
+      header: {
+        left:   'agendaDay, agendaWeek, month',
+        right:  'today, prev, next',
+        center: 'title'
+      }
+    }
+  }
+});
+
+Template.calendar.onRendered(() => {
+  // Override FullCalendar's default look to match the flat look of the dashboard.
+  $('.fc-toolbar button').each(function () {
+    $(this)
+      .removeClass('fc-state-default fc-button fc-corner-left fc-corner-right')
+      .addClass('btn btn-primary');
+
+    if ($(this).hasClass('fc-prev-button')) {
+      $(this).html('<i class="fa fa-angle-left"></i>');
+    } else if ($(this).hasClass('fc-next-button')) {
+      $(this).html('<i class="fa fa-angle-right"></i>');
+    }
+  });
+});
