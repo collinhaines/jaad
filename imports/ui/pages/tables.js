eee7e036a658513afa6c3461a71b2423cda5a14f
Collin Haines <collinhaines@me.com>
2016-09-12 11:56:53 -0400

    Added Tables

--- imports/ui/pages/tables.js
+++ imports/ui/pages/tables.js
@@ -1,1 +1,18 @@
 import './tables.html';
+
+import { Template } from 'meteor/templating';
+
+// Demo only.
+Template.tablePanel.helpers({
+  headerData() {
+    return ['#', 'First Name', 'Last Name', 'Username'];
+  },
+
+  bodyData() {
+    return [
+      { row: ['1', 'Mark', 'Otto', '@mdo'] },
+      { row: ['2', 'Jacob', 'Thornton', '@fat'] },
+      { row: ['3', 'Larry', 'the Bird', '@twitter'] }
+    ]
+  }
+});
