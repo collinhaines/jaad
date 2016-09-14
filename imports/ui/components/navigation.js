a06e89c29386ae5c838f0a0c25c20d76bd398718
Collin Haines <collinhaines@me.com>
2016-09-14 10:48:07 -0400

    Fixes #3

--- imports/ui/components/navigation.js
+++ imports/ui/components/navigation.js
@@ -1,1 +1,19 @@
 import './navigation.html';
+
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Template } from 'meteor/templating';
+
+Template.navigation.onRendered(() => {
+  window.onpopstate = function (event) {
+    // Remove active link.
+    $('.nav li.active')
+      .removeClass('active')
+      .find('> a')
+      .blur();
+
+    // Add active link.
+    $('.nav li a[href="' + FlowRouter.current().path + '"]')
+      .parent()
+      .addClass('active');
+  };
+});
