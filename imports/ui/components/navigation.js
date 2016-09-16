a5a7cdde1fa0f8f43ba2d94fd114ab78101699ac
Collin Haines <collinhaines@me.com>
2016-09-16 07:38:57 -0400

    Assure Sidebar Links

--- imports/ui/components/navigation.js
+++ imports/ui/components/navigation.js
@@ -2,18 +2,36 @@ import './navigation.html';
 
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
 
 Template.navigation.onRendered(() => {
   window.onpopstate = function (event) {
     // Remove active link.
-    $('.nav li.active')
+    $('#sidebar li.active')
       .removeClass('active')
       .find('> a')
       .blur();
 
     // Add active link.
-    $('.nav li a[href="' + FlowRouter.current().path + '"]')
+    $('#sidebar li a[href="' + FlowRouter.current().path + '"]')
       .parent()
       .addClass('active');
   };
+});
+
+Template.navigation.events({
+  'click .navbar-brand'(event) {
+    // Remove active link.
+    $('#sidebar li.active')
+      .removeClass('active')
+      .find('> a')
+      .blur();
+
+    // Add active link.
+    $('#sidebar li a[href="/"]')
+      .parent()
+      .addClass('active');
+
+    event.target.blur();
+  }
 });
--- imports/ui/components/sidebar.js
+++ imports/ui/components/sidebar.js
@@ -1,11 +1,11 @@
+import './sidebar.html';
+
 import { Template } from 'meteor/templating';
 import { $ } from 'meteor/jquery';
 
-import './sidebar.html';
-
 Template.sidebar.onRendered(() => {
   // When rendered add the active class.
-  $('li:not(.nav-category) > a[href="' + FlowRouter.current().route.path + '"]')
+  $('#sidebar li:not(.nav-category) > a[href="' + FlowRouter.current().route.path + '"]')
     .parent()
     .addClass('active');
 });
@@ -13,7 +13,7 @@ Template.sidebar.onRendered(() => {
 Template.sidebar.events({
   'click li:not(.nav-category) > a'(event) {
     // Remove old active.
-    $('li.active').removeClass('active');
+    $('#sidebar li.active').removeClass('active');
 
     // Add new active.
     $(event.target)
